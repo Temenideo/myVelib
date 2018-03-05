f97d2008561dddd3268b917009850ad14b38c710
xavier <xavier@xav-pc.viarezo.fr>
2018-03-05 18:36:08 +0100

    Avoidpresque en cours

--- Velib2/src/ridePolicies/AvoidPlus.java
+++ Velib2/src/ridePolicies/AvoidPlus.java
@@ -7,22 +7,58 @@ import myVelib.Station;
 public class AvoidPlus implements RidePolicy{
 	@Override
 	public Station computeStart(Reseau reseau,GPScoord start,GPScoord end,String typeBike) {
-	double dist=-1;
-	Station startStation = null;
-	for (Station stat : reseau.getStationList()) {
-		if(stat.getState().equals("On service") && stat.getTypeStation()!="Plus") {
-			if(stat.availableBikeE()||stat.availableBikeM()) {
-				if (dist<0 || dist>this.start.getDistance(stat.getPosition())) {
-					dist=this.start.getDistance(stat.getPosition());
-					startStation=stat;
+		double dist=-1;
+		Station startStation = null;
+		if (typeBike=="Electrical"){
+			for (Station stat : reseau.getStationList()) {
+				if(stat.getState().equals("On service")) {
+					if(stat.availableBikeE()) {
+						if (dist<0 || dist>start.getDistance(stat.getPosition())) {
+							dist=start.getDistance(stat.getPosition());
+							startStation=stat;
+						}
+					}
 				}
 			}
 		}
+		if (typeBike=="Mecanical"){
+			for (Station stat : reseau.getStationList()) {
+				if(stat.getState().equals("On service")) {
+					if(stat.availableBikeM()) {
+						if (dist<0 || dist>start.getDistance(stat.getPosition())) {
+							dist=start.getDistance(stat.getPosition());
+							startStation=stat;
+						}
+					}
+				}
+			}
+		}
+		if(startStation!=null) { 
+			return(startStation);
+		}
+		else
+			//il faut aussi return une erreur à coder ensuite
+			System.out.println("No station fitting your criteria is availabale for departure, please try again later or change your ride settings");
 	}
-	if(startStation!=null) { 
-		return(startStation);
+	@Override
+	public Station computeEnd(Reseau reseau,GPScoord start,GPScoord end,String typeBike) {
+		double dist=-1;
+		Station endStation = null;
+		for (Station stat : reseau.getStationList()) {
+			if(stat.getState().equals("On service") && stat.getTypeStation()!="Plus") {
+				if(stat.availableParkingSlot()) {
+					if (dist<0 || dist>start.getDistance(stat.getPosition())) {
+						dist=start.getDistance(stat.getPosition());
+						endStation=stat;
+					}
+				}
+			}
+		}
+		if(endStation!=null) { 
+			return(endStation);
+		}
+		else
+			//il faut aussi return une erreur à coder ensuite
+			System.out.println("No station fitting your criteria is availabale for departure, please try again later or change your ride settings");
 	}
-	else
-		System.out.println("No station fitting your criteria is availabale for departure, please try again later or change your ride settings");
-}
 }
