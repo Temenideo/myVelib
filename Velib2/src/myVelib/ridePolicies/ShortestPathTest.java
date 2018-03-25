d5474d1664f95de18a4d278a6ba0a79306dc40d3
Temenideo <olivier.temenides@student.ecp.fr>
2018-03-25 19:51:17 +0200

    Policies tests

---
+++ Velib2/src/myVelib/ridePolicies/ShortestPathTest.java
@@ -0,0 +1,19 @@
+package myVelib.ridePolicies;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShortestPathTest {
+
+	@Test
+	public void testComputeStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testComputeEnd() {
+		fail("Not yet implemented");
+	}
+
+}
